a1971d1f64967d7436c1aac48ae576f0f37f8931
will_fell <willfellhoelter@gmail.com>
2021-07-18 22:39:00 -0600

    fmt

--- terraform/beans/the_beans.tf
+++ terraform/beans/the_beans.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
+  access_key              = var.access_key
+  secret_key              = var.secret_key
+  region                  = var.region
   shared_credentials_file = "/Users/will/.aws/credentials"
 }
 
@@ -15,9 +15,9 @@ resource "aws_elastic_beanstalk_environment" "beans_env" {
   name                = "node-environment"
   application         = aws_elastic_beanstalk_application.beans_app.name
   solution_stack_name = var.solution_stack
-    setting {
-      namespace = "aws:autoscaling:launchconfiguration"
-      name      = "IamInstanceProfile"
-      value     = "aws-elasticbeanstalk-ec2-role"
-    }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
 }
